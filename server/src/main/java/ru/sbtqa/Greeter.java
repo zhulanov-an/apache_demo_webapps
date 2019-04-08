f354b673e329dcc91d066e9f6867fa391e78d3b6
Anton Zhulanov <zhulanoff2007@gmail.com>
2019-04-08 14:28:47 +0300

    fix checkstyle warnings

--- server/src/main/java/ru/sbtqa/Greeter.java
+++ server/src/main/java/ru/sbtqa/Greeter.java
@@ -1,20 +1,23 @@
 package ru.sbtqa;
 
 /**
- * Greeter class
+ * Greeter class.
  */
 public class Greeter {
 
   /**
-   * Constructor of Greeter class
+   * Constructor of Greeter class.
    */
   public Greeter() {
 
   }
 
 
-
-
+  /**
+   * method greet.
+   * @param name - value of greetings
+   * @return - value "Hello, " + name + "!"
+   */
   public final String greet(final String name) {
     return String.format("Hello, %s!", name);
   }
